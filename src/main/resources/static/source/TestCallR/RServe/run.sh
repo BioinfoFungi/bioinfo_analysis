9a846e08cbe56f9343c2ba0d8ca76609c240febb
wangyang1749 <1749748955@qq.com>
2021-07-19 23:38:25 +0800

    remove some file

--- src/main/resources/static/source/TestCallR/RServe/run.sh
+++
@@ -1,4 +0,0 @@
-javac jt.java -cp .:./REngine.jar
-java -cp .:./REngine.jar:./Rserve.jar jt
-# javac Test.java -cp .:../lib/REngine.jar:../lib/RserveEngine.jar
-# java -cp .:./REngine.jar:../lib/RserveEngine.ja Test
--- src/main/resources/static/source/TestCallR/Rcaller/test.sh
+++
@@ -1,2 +0,0 @@
-javac -cp .:./RCaller-3.0.2-jar-with-dependencies.jar Test.java 
-java -cp .:./RCaller-3.0.2-jar-with-dependencies.jar Test
