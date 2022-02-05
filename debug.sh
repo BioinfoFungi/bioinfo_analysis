6532f20694574ea7552db9ac014b2233172c6f9e
WangYang <1749748955@qq.com>
2022-02-05 10:34:36 +0800

    update

---
+++ debug.sh
@@ -0,0 +1,23 @@
+#!bash
+#git pull
+#./mvnw clean
+# ./mvnw install -Dmaven.test.skip=true
+dir=`pwd`
+app="bioinfo-0.0.1-SNAPSHOT.jar"
+jar="${dir}/target/${app}"
+
+if [ ! -f $jar ];then
+  echo "build failure！！！"
+  exit 8
+fi
+
+pid=$(jps | grep "bioinfo-0.0.1-SNAPSHOT.jar" | awk '{print $1}')
+
+if [ $pid ]
+then
+  echo "kill ${pid}"
+  kill ${pid}
+fi
+
+echo $jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6001 -jar $jar
